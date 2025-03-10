ca8b2d4dd0931b6e84db34e8bc423fd73c016813
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2025-03-10 15:12:31 +0000

    listen for file open requests

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,41 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { IEventListener } from 'jupyterlab-eventlistener';
+import { IDocumentManager } from '@jupyterlab/docmanager';
 
-/**
- * Initialization data for the myextension extension.
- */
+
+// The Jupyter Event schema for the events we are listing to
+const testSchema = 'http://events.cylc.org/test'
+
+
+// The Jupyter Lab extension
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'myextension:plugin',
-  description: 'A JupyterLab extension.',
+  id: 'cylc-extension:plugin',
+  description: 'Listens to Jupyter Lab file open requests from other components.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension myextension is activated!');
+  requires: [
+    // list a token here and it will be provided to the activate function
+    IEventListener,
+    IDocumentManager,
+  ],
+  activate: (
+    app: JupyterFrontEnd,
+    eventListener: IEventListener,
+    documentManager: IDocumentManager,
+  ) => {
+    console.log('JupyterLab extension cylc-extension is activated!');
+
+    async function handleOpenRequest(manager, schemaID, event) {
+      if (event.name === 'open') {
+        documentManager.open(event.path)
+      }
+    }
+
+    eventListener.addListener(
+      testSchema,
+      handleOpenRequest
+    );
   }
 };
 
